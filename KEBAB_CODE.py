2c535eb02e153f7ddb602e10f70868206857f708
Gabriel Moreira <198077215+Gabbers40@users.noreply.github.com>
2026-06-15 08:49:26 -0300

    Update KEBAB_CODE.py

--- KEBAB_CODE.py
+++ KEBAB_CODE.py
@@ -1,11 +1,7 @@
-# Implementar em Python. Não é permitido usar nenhuma função ou biblioteca pronta para cálculo matemático (por exemplo a Num.py): todas as funções deverão ser construídas do zero.
-# O programa deverá ser capaz de:
-# --> receber as dimensões das matrizes, a quantidade de matrizes, e os elementos das matrizes (números reais) através de interface com o usuário;
-# --> perguntar ao usuário, a partir da entrada fornecida, o que ele deseja fazer (por exemplo, se entrou com uma matriz de ordem 2x3, a função de determinante não estará disponível).
-# --> deve ser capaz de calcular: soma/subtração de matrizes, multiplicação de matrizes, multiplicação de matriz por número inteiro, matriz transposta (e cálculos com a transposta gerada), matriz inversa, determinante.
-# --> deverá ser capaz de exportar o resultado.
-# --> IMPORTANTE: O PROGRAMA NÃO DEVE SÓ APRESENTAR O RESULTADO, MAS MOSTRAR O PASSO-A-PASSO DAS OPERAÇÕES.
-# Apresentação/entrega do código, demonstração do programa e resposta a pergunta que o professor irá fazer sobre o código.
+# O que eu fiz:
+# coloquei multiplicação de numero inteiro
+# coloquei multiplicação de matriz
+
 import time
 matrizes = [] # agrupa todas as matrizes
 
